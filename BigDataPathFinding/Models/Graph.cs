4474b6ab7c66c97669481b639abc3997ca46f5ff
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-08-04 17:37:02 +0430

    merger

--- BigDataPathFinding/Models/Graph.cs
+++ BigDataPathFinding/Models/Graph.cs
@@ -8,18 +8,39 @@ namespace BigDataPathFinding.Models
         public Dictionary<Guid, ResultNode> nodes { get; } = new Dictionary<Guid, ResultNode>();
         public HashSet<Edge> edges { get; } = new HashSet<Edge>();
 
-        public void AddEdge(Guid source, Guid target, double weight) => AddEdge(GetNode(source), GetNode(target), weight);
+        public void AddEdge(Guid source, Guid target, double weight)
+        {
+            AddEdge(GetNode(source), GetNode(target), weight);
+        }
 
-        private ResultNode GetNode(Guid id) => nodes?[id];
+        private ResultNode GetNode(Guid id)
+        {
+            return nodes?[id];
+        }
 
-        public bool ContainsNode(Guid id) => nodes.ContainsKey(id);
+        public bool ContainsNode(Guid id)
+        {
+            return nodes.ContainsKey(id);
+        }
 
-        public void AddNode(ResultNode node) => nodes[node.Id] = node;
+        public void AddNode(ResultNode node)
+        {
+            nodes[node.Id] = node;
+        }
 
-        public void AddEdge(ResultNode source, ResultNode target, double weight) => edges.Add(new Edge(source.Id, target.Id, weight));
+        public void AddEdge(ResultNode source, ResultNode target, double weight)
+        {
+            edges.Add(new Edge(source.Id, target.Id, weight));
+        }
 
-        public bool Explored(Guid id) => ContainsNode(id) && nodes[id].Explored;
+        public bool Explored(Guid id)
+        {
+            return ContainsNode(id) && nodes[id].Explored;
+        }
 
-        public void Explore(Guid id) => GetNode(id).Explored = true;
+        public void Explore(Guid id)
+        {
+            GetNode(id).Explored = true;
+        }
     }
 }
--- BigDataPathFinding/Models/ResultNode.cs
+++ BigDataPathFinding/Models/ResultNode.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BigDataPathFinding.Models
+﻿namespace BigDataPathFinding.Models
 {
     public class ResultNode : Node
     {
-        public bool Explored { get; set; } = false;
-
         public ResultNode(Node node) : base(node.Id, node.Name)
         {
         }
+
+        public bool Explored { get; set; } = false;
     }
 }
