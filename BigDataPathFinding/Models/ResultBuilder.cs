634a9bca7c35855e472016ae0c4bf4279dedcd01
ahmadsalimi <ahsa9978@gmail.com>
2019-08-04 16:54:23 +0430

    implement ResultBuilder

--- BigDataPathFinding/Models/ResultBuilder.cs
+++ BigDataPathFinding/Models/ResultBuilder.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BigDataPathFinding.Models
 {
     public class ResultBuilder
     {
         private readonly IDatabase _database;
-        private readonly Dictionary<Guid, NodeData> _nodeSet = new Dictionary<Guid, NodeData>();
+        private readonly Dictionary<Guid, NodeData> _nodeSet;
         private readonly Graph _result = new Graph();
 
         public ResultBuilder(IDatabase database, Dictionary<Guid, NodeData> nodeSet)
@@ -18,8 +16,40 @@ namespace BigDataPathFinding.Models
             _nodeSet = nodeSet;
         }
 
-        public Graph Build()
+        public Graph Build(Guid targetId)
         {
+            if (!_nodeSet.ContainsKey(targetId))
+            {
+                return _result;
+            }
+
+            var currentNodes = new HashSet<NodeData> { _nodeSet[targetId] };
+
+            while (currentNodes.Count > 0)
+            {
+                var node = currentNodes.First();
+                currentNodes.Remove(node);
+
+                if (_result.Explored(node.Id)) continue;
+                _result.Explore(node.Id);
+
+                if (!_result.ContainsNode(node.Id))
+                {
+                    _result.AddNode(new ResultNode(_database.GetNode(node.Id)));
+                }
+
+                foreach (var adjacent in node.PreviousAdjacents)
+                {
+                    if (!_result.ContainsNode(adjacent.Id))
+                    {
+                        _result.AddNode(new ResultNode(_database.GetNode(adjacent.Id)));
+                    }
+
+                    _result.AddEdge(node.Id, adjacent.Id, adjacent.Weight);
+                    currentNodes.Add(_nodeSet[adjacent.Id]);
+                }
+            }
+
             return _result;
         }
     }
