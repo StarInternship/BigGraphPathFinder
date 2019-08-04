4185c908fe580834fb5123723e6d8285ccadce89
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-08-04 15:18:22 +0430

    Mahdi PathFinder

--- BigDataPathFinding/Models/Adjacent.cs
+++ BigDataPathFinding/Models/Adjacent.cs
@@ -4,13 +4,13 @@ namespace BigDataPathFinding.Models
 {
     public class Adjacent
     {
-        public Guid Id { get; }
-        public double Weight { get; }
-
         public Adjacent(Guid id, double weight)
         {
             Id = id;
             Weight = weight;
         }
+
+        public Guid Id { get; }
+        public double Weight { get; }
     }
 }
--- BigDataPathFinding/Models/Edge.cs
+++ BigDataPathFinding/Models/Edge.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace BigDataPathFinding.Models
 {
     public class Edge
     {
-        public Guid SourceId { get; }
-        public Guid TargetId { get; }
-        public double Weight { get; }
-
         public Edge(Guid sourceId, Guid targetId, double weight)
         {
             SourceId = sourceId;
             TargetId = targetId;
             Weight = weight;
         }
+
+        public Guid SourceId { get; }
+        public Guid TargetId { get; }
+        public double Weight { get; }
 
         //TODO: override methods
     }
