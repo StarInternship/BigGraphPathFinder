00401c09869c35f40a3c06821eb990fe5fee3931
ahmadsalimi <ahsa9978@gmail.com>
2019-08-04 15:34:17 +0430

    add database to pathFinder

--- BigDataPathFinding/Models/PathFinder.cs
+++ BigDataPathFinding/Models/PathFinder.cs
@@ -4,13 +4,15 @@ namespace BigDataPathFinding.Models
 {
     public abstract class PathFinder
     {
-        protected readonly bool Directed;
+        protected readonly IDatabase Database;
         protected readonly IMetadata Metadata;
         protected readonly Guid SourceId;
         protected readonly Guid TargetId;
+        protected readonly bool Directed;
 
-        protected PathFinder(IMetadata metadata, Guid sourceId, Guid targetId, bool directed)
+        protected PathFinder(IDatabase database, IMetadata metadata, Guid sourceId, Guid targetId, bool directed)
         {
+            Database = database;
             Metadata = metadata;
             SourceId = sourceId;
             TargetId = targetId;
