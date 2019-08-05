6b0bdfa40304496168740f066f3cb2cdddb201cc
ahmadsalimi <ahsa9978@gmail.com>
2019-08-06 00:17:15 +0430

    make ElasticDatabase class

---
+++ BigDataPathFinding/Models/ElasticDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+
+namespace BigDataPathFinding.Models
+{
+    public class ElasticDatabase : IDatabase
+    {
+        private static readonly Uri Uri = new Uri($"http://localhost:9200");
+        private readonly ElasticClient _client;
+
+        public ElasticDatabase(string nodeSetIndex)
+        {
+            var settings = new ConnectionSettings(Uri).DefaultIndex(nodeSetIndex);
+            _client = new ElasticClient(settings);
+        }
+
+        public Node GetNode(Guid id) // I THINK IT'S NOT GOOD AT ALL.
+        {
+            var searchResponse = _client.Search<Node>(s => s.Query(q => q.Match(m => m.Field(t => t.Id).Query(id.ToString()))));
+            return searchResponse.Documents.First();
+        }
+    }
+}
