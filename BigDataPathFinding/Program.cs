15c3fecd3c674ee402107821796027865832fb3f
ahmadsalimi <ahsa9978@gmail.com>
2019-08-05 14:38:10 +0430

    change program.cs

--- BigDataPathFinding/Program.cs
+++ BigDataPathFinding/Program.cs
@@ -14,43 +14,48 @@ namespace BigDataPathFinding
             const string testFilesPath = @"../../../TestFiles/";
             var database = new FileGraph(testFilesPath + "BigGraphAllpathSearch.csv");
             var metadata = new FileMetadata(database);
-            var sourceId = database.GetId("142");
-            var targetId = database.GetId("975");
-            var directed = true;
 
-            var stopWatch = new Stopwatch();
+            while (true)
+            {
+                Console.Write("source: ");
+                var sourceId = database.GetId(Console.ReadLine());
+                Console.Write("target: ");
+                var targetId = database.GetId(Console.ReadLine());
+                Console.Write("directed(1 or 0): ");
+                var directed = Console.ReadLine() != "0";
 
-            stopWatch.Start();
+                var stopWatch = new Stopwatch();
 
-            PathFinder pathFinder = new HadiPathFinder(metadata, sourceId, targetId, directed);
-            pathFinder.FindPath();
-            stopWatch.Stop();
-            Console.WriteLine("Finding Path Finished In " + stopWatch.ElapsedMilliseconds + "ms.");
-            stopWatch.Reset();
-            stopWatch.Start();
-            var resultBuilder = new ResultBuilder(database, pathFinder.GetResultNodeSet());
-            var actual = resultBuilder.Build(targetId).Edges;
-            Console.WriteLine("Generating Graph Finished In " + stopWatch.ElapsedMilliseconds + "ms.");
-            foreach (var edge in actual) Console.WriteLine(edge);
-            Console.WriteLine("******** Hadi ********\n\n");
+                stopWatch.Start();
+
+                PathFinder pathFinder = new HadiPathFinder(metadata, sourceId, targetId, directed);
+                pathFinder.FindPath();
+                stopWatch.Stop();
+                Console.WriteLine("Finding Path Finished In " + stopWatch.ElapsedMilliseconds + "ms.");
+                stopWatch.Reset();
+                stopWatch.Start();
+                var resultBuilder = new ResultBuilder(database, pathFinder.GetResultNodeSet());
+                var actual = resultBuilder.Build(targetId).Edges;
+                Console.WriteLine("Generating Graph Finished In " + stopWatch.ElapsedMilliseconds + "ms.");
+                foreach (var edge in actual) Console.WriteLine(edge);
+                Console.WriteLine("******** Hadi ********\n\n");
 
 
-            pathFinder = new MahdiPathFinder(metadata, sourceId, targetId, directed);
-            pathFinder.FindPath();
-            stopWatch.Stop();
-            Console.WriteLine("Finding Path Finished In " + stopWatch.ElapsedMilliseconds + "ms.");
-            stopWatch.Reset();
-            stopWatch.Start();
-            resultBuilder = new ResultBuilder(database, pathFinder.GetResultNodeSet());
-            actual = resultBuilder.Build(targetId).Edges;
-            Console.WriteLine("Generating Graph Finished In " + stopWatch.ElapsedMilliseconds + "ms.");
-            foreach (var edge in actual) Console.WriteLine(edge);
-            Console.WriteLine("******* Mahdi *********");
+                pathFinder = new MahdiPathFinder(metadata, sourceId, targetId, directed);
+                pathFinder.FindPath();
+                stopWatch.Stop();
+                Console.WriteLine("Finding Path Finished In " + stopWatch.ElapsedMilliseconds + "ms.");
+                stopWatch.Reset();
+                stopWatch.Start();
+                resultBuilder = new ResultBuilder(database, pathFinder.GetResultNodeSet());
+                actual = resultBuilder.Build(targetId).Edges;
+                Console.WriteLine("Generating Graph Finished In " + stopWatch.ElapsedMilliseconds + "ms.");
+                foreach (var edge in actual) Console.WriteLine(edge);
+                Console.WriteLine("******* Mahdi *********");
 
-            stopWatch.Reset();
-            stopWatch.Start();
-
-            Console.ReadKey();
+                stopWatch.Reset();
+                stopWatch.Start();
+            }
         }
     }
 }
